f586cc9690a72c0b95603cbfadff74126c389bd1
aditya200 <aditya@revidd.com>
2022-11-27 15:35:01 +0530

    themecontext added and header fixed to lighttodark

--- smac-frontend/src/header/Header.js
+++ smac-frontend/src/header/Header.js
@@ -1,22 +1,53 @@
-import React from 'react'
+import React, { useContext } from "react";
 
-import { Button } from '@material-ui/core'
-import { Avatar } from '@mui/material'
+import { ThemeContext } from "../theme/themeContext";
 
-import logoIcon from "../icons/logo.svg"
-import sunIcon from "../icons/sun.svg"
-import bellIcon from "../icons/bell.svg"
+import { Button } from "@material-ui/core";
+import { Avatar } from "@mui/material";
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+
+import logoIcon from "../icons/logo.svg";
+import sunIcon from "../icons/sun.svg";
+import logoInLightIcon from "../icons/logoInLightMode.svg";
 
 export default function Header() {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  const handleTheme = () =>
+    theme === "dark" ? setTheme("light") : setTheme("dark");
+
   return (
-    <div className='flex items-center bg-headerBGLight'>
-      <div className='flex-1'>
-     <img src={logoIcon} alt="logo" className='my-5 ml-24 h-[35px]'/>
-     </div>
-      <button style={{textTransform:"none"}} className="text-fontButtonPink font-bold text-base bg-buttonBg py-4 px-3 rounded-[13px]">Contact us</button>
-     <img src={sunIcon} alt="logo" className='mx-6'/>
-     <img src={bellIcon} alt="logo" className='mr-6'/>
-     <Avatar src="https://assets-prd.ignimgs.com/2022/09/20/iron-man-motive-button-1663693168356.jpg" sx={{width:"64px", height:"64px"}} className="mr-12"/>
+    <div className="flex items-center dark:bg-white bg-headerBGLight">
+      <div className="flex-1">
+        {theme === "light" ? (
+          <img src={logoIcon} alt="logo" className="my-5 ml-24 h-[35px]" />
+        ) : (
+          <img
+            src={logoInLightIcon}
+            alt="logo"
+            className="my-5 ml-24 h-[35px]"
+          />
+        )}
+      </div>
+
+      <button
+        style={{ textTransform: "none" }}
+        className="text-fontButtonPink font-bold text-base bg-buttonBg py-4 px-3 rounded-[13px]"
+      >
+        Contact us
+      </button>
+
+      <Button onClick={handleTheme}>
+        <img src={sunIcon} alt="logo" className="mx-6" />
+      </Button>
+
+      <NotificationsNoneIcon className="mr-6 text-white dark:text-black" />
+
+      <Avatar
+        src="https://assets-prd.ignimgs.com/2022/09/20/iron-man-motive-button-1663693168356.jpg"
+        sx={{ width: "64px", height: "64px" }}
+        className="mr-12"
+      />
     </div>
-  )
+  );
 }
--- smac-frontend/tailwind.config.js
+++ smac-frontend/tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors:{
-        fontButtonPink:"#F19CFF",
-        buttonBg:"#060B1D",
-        headerBGLight:"#53504D"
-      }
+      colors: {
+        fontButtonPink: "#F19CFF",
+        buttonBg: "#060B1D",
+        headerBGLight: "#53504D",
+      },
     },
   },
   plugins: [],
-}
+};
